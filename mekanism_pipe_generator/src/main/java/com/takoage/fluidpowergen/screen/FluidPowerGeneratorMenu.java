7b5d8b77d4aff05f320c5698889f83f0ce294cbb
Haruki Kanzaki <B2678901@eagle.sophia.ac.jp>
2026-04-12 14:45:41 +0900

    Create FluidPowerGeneratorMenu.java container class for GUI menu

---
+++ mekanism_pipe_generator/src/main/java/com/takoage/fluidpowergen/screen/FluidPowerGeneratorMenu.java
@@ -0,0 +1,14 @@
+package com.takoage.fluidpowergen.screen;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.network.FriendlyByteBuf;
+
+public class FluidPowerGeneratorMenu {
+    // GUI menu implementation goes here
+
+    public FluidPowerGeneratorMenu(int id, Inventory playerInventory, FriendlyByteBuf buf) {
+        // Constructor implementation
+    }
+
+    // Additional methods and logic for the FluidPowerGeneratorMenu
+}
